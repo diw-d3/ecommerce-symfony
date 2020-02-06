d93dcd877540a92280a061bb4032881859d0bfe9
Matthieu Mota <matthieumota@gmail.com>
2020-02-06 12:55:53 +0100

    Add base layout and homepage

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function home()
+    {
+        return $this->render('index/home.html.twig');
+    }
+}
