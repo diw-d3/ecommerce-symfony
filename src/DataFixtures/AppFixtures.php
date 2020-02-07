67a42a10a50d70e1177de5189317e305349415e6
Matthieu Mota <matthieumota@gmail.com>
2020-02-07 13:39:13 +0100

    Image on products

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,12 +7,28 @@ use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var HttpClientInterface
+     */
+    private $client;
+
+    private $uploadDir;
+
+    public function __construct(HttpClientInterface $client, $uploadDir)
+    {
+        $this->client = $client;
+        $this->uploadDir = $uploadDir;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
+        $fs = new Filesystem();
 
         $categories = [];
         for ($i = 0; $i < 5; ++$i) {
@@ -24,7 +40,7 @@ class AppFixtures extends Fixture
         }
 
         $colors = ['Bleu', 'Rouge', 'Vert'];
-        for ($i = 0; $i < 100; ++$i) {
+        for ($i = 0; $i < 10; ++$i) {
             $product = new Product();
             $product->setName('Produit '.$i);
             $product->setSlug('produit-'.$i);
@@ -33,7 +49,9 @@ class AppFixtures extends Fixture
             $product->setCreatedAt($faker->dateTimeBetween('-30 days'));
             $product->setIsHeartStroke($faker->boolean);
             $product->setColors($faker->randomElements($colors, rand(0, count($colors))));
-            $product->setImage(null);
+            $response = $this->client->request('GET', 'https://picsum.photos/400/400');
+            $fs->dumpFile($this->uploadDir.'/'.$product->getSlug().'.jpg', $response->getContent());
+            $product->setImage($product->getSlug().'.jpg');
             $product->setDiscount($faker->numberBetween(0, 90));
             $product->setCategory($faker->randomElement($categories));
             $manager->persist($product);
